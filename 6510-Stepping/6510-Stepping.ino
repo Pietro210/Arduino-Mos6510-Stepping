bdb08686f1f4ee902f2ff6c0f98d20671b311193
Pietro <pietro210@hotmail.it>
2019-06-22 02:00:00 +0200

    Update sketch

--- 6510-Stepping/6510-Stepping.ino
+++ 6510-Stepping/6510-Stepping.ino
@@ -4,184 +4,182 @@
  * (c) 2014 Robert Crossfield
  **/
 
-int pinClock = 04;
-int pinReady = 22;
-int pinReset = 23;
-int pinRW    = 48;
+#include "memory.h"
 
-int pinsAddress[16] = {24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39};
-int pinsData[8]     = {40, 41, 42, 43, 44, 45, 46, 47};
+int pinClock = 5;
+int pinReady = 2;
+int pinReset = 3;
+int pinRW = 4;
+int pinsData[8] = { 6, 7, 8, 9, 10, 11, 12, 13 };
 
-int previousAddress = 0;
-bool previousRW = false;
+bool autorun = false;
+bool wasWrite = false;
+uint16_t previousAddress = 0;
 
 String commandInput = "";
 boolean commandComplete = false;
 
-const unsigned char Memory[ 0x20 ] = { 0x00, 0x00, 0x78, 0x4c, 0x03, 0x00, 0x00, 0x00, 0x00, 0x78, 0x4C, 0x09, 0x00, 0xA9, 0x04 };
+uint16_t getAddress() {
+  uint16_t address = 0;
 
-void setup() {
-  commandInput.reserve(200);
-  
-  Serial.begin(9600);
-  
-  // Timer0 frequency set to 62500 Hz
-  //TCCR0B = TCCR0B & 0b11111000 | 0x01;
-  
-  pinMode(pinClock, OUTPUT);
-  pinMode(pinReady, OUTPUT);
-  pinMode(pinReset, OUTPUT);
-  pinMode(pinRW,    INPUT);
-    
-  for( int pin = 0; pin < 8; ++pin )
-    pinMode(pinsData[pin], OUTPUT);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
 
-  setData(0x00);
-  
-  digitalWrite( pinReady, LOW );
-  digitalWrite( pinReset, HIGH );
-  
-  analogWrite( pinClock, 127 );  // 50% on, 50% off
+  bitWrite(address, 0, digitalRead(A0));
+  bitWrite(address, 1, digitalRead(A1));
+  bitWrite(address, 2, digitalRead(A2));
+  bitWrite(address, 3, digitalRead(A3));
+  bitWrite(address, 4, digitalRead(A4));
+  bitWrite(address, 5, digitalRead(A5));
+
+  return address;
 }
 
-/**
- * Handle serial port event
- **/
+uint8_t getData() {
+  for (int pin = 0; pin < 8; pin++) {
+    pinMode(pinsData[pin], INPUT);
+  }
+
+  uint8_t data;
+
+  for (int pin = 0; pin < 8; pin++) {
+    bitWrite(data, pin, digitalRead(pinsData[pin]));
+  }
+
+  return data;
+}
+
+void setData(uint8_t data) {
+  for (int pin = 0; pin < 8; pin++) {
+    pinMode(pinsData[pin], OUTPUT);
+  }
+
+  for (int pin = 0; pin < 8; pin++) {
+    if (data & 1) {
+      digitalWrite(pinsData[pin], HIGH);
+    }
+    else {
+      digitalWrite(pinsData[pin], LOW);
+    }
+    data >>= 1;
+  }
+}
+
+void reset() {
+  digitalWrite(pinReset, LOW);
+  delay(10);
+  digitalWrite(pinReset, HIGH);
+  Serial.println("reset");
+}
+
+void step() {
+  digitalWrite(pinReady, HIGH);
+  delay(1);
+  digitalWrite(pinReady, LOW);
+}
+
 void serialEvent() {
   while (Serial.available()) {
     char inChar = (char)Serial.read();
 
     commandInput += inChar;
-    if (inChar == '\n')
+    if (inChar == '\n') {
       commandComplete = true;
-  }
-}
-
-/**
- * Get the address pins
- **/
-unsigned short getAddress() {
-  unsigned short Address = 0;
-  
-  for( int pin = 0; pin < 16; ++pin )
-    pinMode(pinsAddress[pin], INPUT);
-    
-  for( int pin = 0; pin < 16; ++pin ) {
-    
-      if( digitalRead( pinsAddress[pin] ) == HIGH )
-        Address |= (1 << pin);
-  }
-  
-  return Address;
-}
-
-/**
- * Set the data line
- **/
-void setData( unsigned char pData ) {
-  
-    for( int pin = 0; pin < 8; ++pin ) {
-      if( pData & 1 )
-        digitalWrite( pinsData[pin], HIGH );
-      else
-        digitalWrite( pinsData[pin], LOW );
-        
-        pData >>= 1;
     }
-}
-
-/** 
- * Reset The CPU
- **/
-void Reset() {
-    digitalWrite( pinReset, LOW );
-    delay(10);
-    digitalWrite( pinReset, HIGH );
-}
-
-/**
- * Single Step
- */ 
-void Step() {
-  digitalWrite( pinReady, HIGH );
-  delay(1);
-  digitalWrite( pinReady, LOW );
-}
-
-/**
- * Get the READ/WRITE Pin status
- *
- * @return True if READING, False if WRITTING
- **/ 
-bool GetRW() {
-  if( digitalRead( pinRW ) == HIGH )
-    return true;
-  
-  return false;  
-}
-
-/**
- * Main Loop
- **/
-void loop(){
-  
-  // Get current address pins
-  unsigned short Address = getAddress();
-  
-  // Get current Read/Write Status
-  bool RW = GetRW();
-  
-  // Has the read/write pin changed since the last loop
-  if( RW != previousRW ) {
-    if( RW == true )
-      Serial.print("READ  ");
-    else
-      Serial.print("WRITE ");
-      previousRW = RW;
   }
-  
-  // Has the address changed since the last loop
-  if( Address != previousAddress ) {
-    Serial.print("Address: 0x");
-    Serial.print( Address, HEX );
+}
 
-    previousAddress = Address;
-  
-    if( RW == true ) {
-      unsigned char Data;
-      
-      switch( Address ) {
-        case 0xFFFC:
-          Data = 0x02;
-          break;
-          
-        case 0xFFFD:
-          Data = 0x00;
-          break;
-          
-        default:
-          Data = Memory[ Address ];
-          break;
-      }
-      Serial.print(" Data: ");
-      Serial.print(Data, HEX );
-      
-      setData( Data ); 
+void setup() {
+  commandInput.reserve(16);
+
+  Serial.begin(9600);
+  Serial.println("start");
+
+  pinMode(pinClock, OUTPUT);
+  pinMode(pinReady, OUTPUT);
+  pinMode(pinReset, OUTPUT);
+  pinMode(pinRW, INPUT);
+
+  digitalWrite(pinReady, LOW);
+  digitalWrite(pinReset, HIGH);
+
+  analogWrite(pinClock, 127); // 50% on, 50% off
+}
+
+void loop() {
+  bool isWrite = digitalRead(pinRW) == LOW;
+  uint16_t address = getAddress();
+  uint8_t data;
+  char buff[16];
+
+  if (isWrite) {
+    data = getData();
+  }
+  else {
+    // READ 0xfffc
+    if (address == 0x3c) {
+      data = 0x02;
     }
-    Serial.println("");
+    // READ 0xfffd
+    else if (address == 0x3d) {
+      data = 0x00;
+    }
+    else {
+      data = memory[address];
+    }
+
+    setData(data);
   }
-  delay(10);
-  
-  // Check for commands from serial port
-   if( commandComplete == true ) {
+
+  if (isWrite != wasWrite || address != previousAddress) {
+    if (isWrite) {
+      Serial.print('W');
+    }
+    else {
+      Serial.print('R');
+    }
+
+    sprintf(buff, " 0x%04x", address);
+    Serial.print(buff);
+
+    Serial.print(" data: ");
+
+    sprintf(buff, "0x%02x", data);
+    Serial.print(buff);
+    Serial.println();
+  }
+
+  wasWrite = isWrite;
+  previousAddress = address;
+
+  if (commandComplete == true) {
     commandComplete = false;
-    
-    if( commandInput == "r\n" )
-      Reset();
-    if( commandInput == "z\n" )
-        Step();
-        
+
+    if (commandInput == "r\n") {
+      reset();
+    }
+    if (commandInput == "z\n") {
+      step();
+    }
+    if (commandInput == "a\n") {
+      autorun = true;
+    }
+    if (commandInput == "s\n") {
+      autorun = false;
+    }
+
     commandInput = "";
   }
-}
 
+  if (autorun) {
+    step();
+    delay(100);
+  }
+  else {
+    delay(10);
+  }
+}
